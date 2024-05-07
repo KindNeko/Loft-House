61013ae540bddf8ae314ba5959f22d1451306016
Maxim Pantelidi <maksim.pantelidi@yandex.ru>
2024-05-07 23:20:43 +0300

    header mobile

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,10 @@
+const menuBtn = toggleMenu(".menu__btn");
+const menuList = document.querySelector(".menu__list");
+const overlay = document.querySelector(".menu-overlay");
 
+function toggleMenu(selector) {
+  document.querySelector(selector).addEventListener("click", () => {
+    menuList.classList.toggle("menu__list--open");
+    overlay.classList.toggle("menu-overlay--show");
+  });
+}
