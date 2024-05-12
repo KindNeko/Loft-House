df2cd35f76f32366b787a920999e88e7a165e5c7
Maxim Pantelidi <maksim.pantelidi@yandex.ru>
2024-05-13 01:35:46 +0300

    footer mobile

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,17 @@
+const menuBtn = toggleMenu(".menu__btn");
+const menuList = document.querySelector(".menu__list");
+const overlay = document.querySelector(".menu-overlay");
+const faqs = document.querySelectorAll(".accordion__faq");
 
+function toggleMenu(selector) {
+  document.querySelector(selector).addEventListener("click", () => {
+    menuList.classList.toggle("menu__list--open");
+    overlay.classList.toggle("menu-overlay--show");
+  });
+}
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("active");
+  });
+});
