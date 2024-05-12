f34f44e3f2660a7b8ffe8a7dff357abfb88acd17
Maxim Pantelidi <maksim.pantelidi@yandex.ru>
2024-05-13 01:40:41 +0300

    init js

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 const menuBtn = toggleMenu(".menu__btn");
 const menuList = document.querySelector(".menu__list");
 const overlay = document.querySelector(".menu-overlay");
+const faqs = document.querySelectorAll(".accordion__faq");
 
 function toggleMenu(selector) {
   document.querySelector(selector).addEventListener("click", () => {
@@ -8,3 +9,9 @@ function toggleMenu(selector) {
     overlay.classList.toggle("menu-overlay--show");
   });
 }
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("active");
+  });
+});
