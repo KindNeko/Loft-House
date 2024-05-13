96dd713439578c7ef1b272a9b83c78c42ca49053
Maxim Pantelidi <maksim.pantelidi@yandex.ru>
2024-05-13 21:25:08 +0300

    меню бургер и аккардеон

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,51 @@
-const menuBtn = toggleMenu(".menu__btn");
-const menuList = document.querySelector(".menu__list");
-const overlay = document.querySelector(".menu-overlay");
-const faqs = document.querySelectorAll(".accordion__faq");
-
-function toggleMenu(selector) {
-  document.querySelector(selector).addEventListener("click", () => {
-    menuList.classList.toggle("menu__list--open");
-    overlay.classList.toggle("menu-overlay--show");
-  });
+"use strict";
+class Toggle {
+  list;
+  open;
+  constructor(list, open) {
+    this.list = list;
+    this.open = open;
+  }
+  toggle() {
+    console.log("default implementation");
+  }
 }
 
-faqs.forEach((faq) => {
-  faq.addEventListener("click", () => {
-    faq.classList.toggle("active");
-  });
-});
+class Menu extends Toggle {
+  overlay = document.querySelector(".menu-overlay");
+  overlayShow = "menu-overlay--show";
+  menuList = document.querySelector(".menu__list");
+  menuOpen = "menu__list--open";
+  constructor(list, open) {
+    super();
+    this.list = list;
+    this.open = open;
+  }
+  toggle(selector) {
+    document.querySelector(selector).addEventListener("click", () => {
+      this.menuList.classList.toggle(this.menuOpen);
+      this.overlay.classList.toggle(this.overlayShow);
+    });
+  }
+}
+const menu = new Menu(Menu.listItem, Menu.openItem);
+menu.toggle(".menu__btn");
+
+class Accordeon extends Toggle {
+  accordeonList = document.querySelectorAll(".accordeon__faq");
+  accordeonOpen = "active";
+  constructor(list, open) {
+    super();
+    this.list = list;
+    this.open = open;
+  }
+
+  toggle() {
+    this.accordeonList.forEach((faq) =>
+      faq.addEventListener("click", () => {
+        faq.classList.toggle(this.accordeonOpen);
+      })
+    );
+  }
+}
+new Accordeon(Accordeon.accordeonList, Accordeon.accordeonOpen).toggle();
