5141a3b4821da863ef0c943bd133b473819326ef
Maxim Pantelidi <maksim.pantelidi@yandex.ru>
2024-05-14 12:40:15 +0300

    дописал класс Menu добавив 2 метода, добавил карту на сайт

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,6 @@
 "use strict";
+
+const header = document.querySelector(".header");
 class Toggle {
   list;
   open;
@@ -6,16 +8,16 @@ class Toggle {
     this.list = list;
     this.open = open;
   }
-  toggle() {
-    console.log("default implementation");
-  }
+  toggle() {}
 }
 
 class Menu extends Toggle {
   overlay = document.querySelector(".menu-overlay");
   overlayShow = "menu-overlay--show";
+  menu = document.querySelector(".menu");
   menuList = document.querySelector(".menu__list");
   menuOpen = "menu__list--open";
+  menuLink = document.querySelectorAll(".menu__link");
   constructor(list, open) {
     super();
     this.list = list;
@@ -27,9 +29,53 @@ class Menu extends Toggle {
       this.overlay.classList.toggle(this.overlayShow);
     });
   }
+
+  /**
+   * @namespace menuListEvent - метод реализующий делегирование событий
+   * Для делегирования я добавил слушатель событий клика на menuList.
+   * e.target - получение элементов на которые сработало событие.
+   * С помощью проверки нахожу элементы с нужным классом.
+   * @param callback - параметр который принимает метод с использованием события на дочерние элементы menuList.
+   */
+  menuListEvent(callback) {
+    this.menuList.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (e.target.classList.contains("menu__link")) {
+        callback(e);
+      }
+    });
+  }
+
+  /**
+   * @namespace smoothScrolling - метод плавного скролла при клике на пункты меню.
+   * В методе используется делегирование событий c помощью метода @namespace menuListEvent.
+   */
+  smoothScrolling() {
+    this.menuList.addEventListener("click", (e) => {
+      e.preventDefault();
+      const href = e.target.getAttribute("href");
+      document.querySelector(href).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  }
+
+  /**
+   * @namespace removeStyle - метод скрывающий меню-бургер при клике на пункты меню.
+   * В методе используется делегирование событий c помощью метода @namespace menuListEvent.
+   */
+  removeStyle() {
+    this.menuList.addEventListener("click", (e) => {
+      this.menuList.classList.remove(this.menuOpen);
+      this.overlay.classList.remove(this.overlayShow);
+    });
+  }
 }
-const menu = new Menu(Menu.listItem, Menu.openItem);
-menu.toggle(".menu__btn");
+const menuBtn = new Menu(Menu.listItem, Menu.openItem);
+menuBtn.toggle(".menu__btn");
+
+new Menu().menuListEvent(new Menu().smoothScrolling());
+new Menu().menuListEvent(new Menu().removeStyle());
 
 class Accordeon extends Toggle {
   accordeonList = document.querySelectorAll(".accordeon__faq");
@@ -49,3 +95,16 @@ class Accordeon extends Toggle {
   }
 }
 new Accordeon(Accordeon.accordeonList, Accordeon.accordeonOpen).toggle();
+
+function init() {
+  let map = new ymaps.Map("map", {
+    center: [59.943543, 30.338928],
+    zoom: 16,
+    controls: [],
+  });
+
+  let myPlacemark = new ymaps.Placemark([59.943543, 30.338928]);
+  map.geoObjects.add(myPlacemark);
+}
+
+ymaps.ready(init);
